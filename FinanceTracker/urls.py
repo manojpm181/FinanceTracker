12f0fc56ac13899709c6cdd937ea7c712ffd0b01
Manoj P M <159712848+manojpm181@users.noreply.github.com>
2024-08-02 11:11:38 +0530

    main

---
+++ FinanceTracker/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', include('FinTech.urls')),
+    path('admin/', admin.site.urls),
+]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
